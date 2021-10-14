ee90980832b3065f8a5a0b2e0e3208d19d47ac5b
davidberger2448@gmail.com <davidberger2448@gmail.com>
2021-10-14 16:04:27 +0300

    added most Structures..

--- dotNet5782_1053_2057/DAL/Class1.cs
+++ dotNet5782_1053_2057/DAL/Class1.cs
@@ -1,19 +1,51 @@
 ﻿using System;
 
-namespace DAL
-{
-    
-}
 namespace IDAL
 {
     namespace DO
     {
-        class MyClass
-        {
+        enum WeightCategories    { light, medium, heavy };
+        enum DroneStatus         { available, work_in_progress, sent};
+        enum Priorities          { regular, fast, urgent};
 
+
+        struct Drone
+        {
+            int id;
+            string model;
+            WeightCategories MaxWeight;
+            DroneStatus status;
+            double battery;
+        }
+        struct Customer
+        {
+            int id;
+            string name;
+            string phone;
+            double longitude;
+            double latitude;
+        }
+        struct Parcel
+        {
+            int id;
+            int senderId;
+            int targetId;
+            WeightCategories weight;
+            Priorities priority;
+            DateTime requested;
+            int droneId;
+            DateTime scheduled;
+            DateTime pickup;
+            DateTime delivered;
         }
 
+        struct DroneCharge
+        {
+            int droneId;
+            int stationId;
+        }
 
+       
 
     }
 }
